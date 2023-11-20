e8e8a588e865a93e52094cd8e2b8a9168c6defbf
j2 <juan@genso.com.bo>
2023-11-20 16:48:03 -0600

    Add exception handling support

--- src/main/java/com/diplomado/homework/web/rest/RoleController.java
+++ src/main/java/com/diplomado/homework/web/rest/RoleController.java
@@ -4,8 +4,10 @@ import com.diplomado.homework.domain.entities.Role;
 import com.diplomado.homework.dto.RoleDTO;
 import com.diplomado.homework.dto.RoleWithDetailDTO;
 import com.diplomado.homework.services.RoleService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -28,49 +30,80 @@ public class RoleController {
 
     @GetMapping("/{id}/users")
     public ResponseEntity<RoleWithDetailDTO> listUsersForRole(@PathVariable final Long id) {
-        return ResponseEntity
-                .ok()
-                .body(roleService.listUsersForRole(id));
+        try {
+            return ResponseEntity
+                    .ok()
+                    .body(roleService.listUsersForRole(id));
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<RoleDTO> getRoleById(@PathVariable final Long id) {
-        return ResponseEntity
-                .ok()
-                .body(roleService.getRoleById(id).orElseThrow(() -> new IllegalArgumentException("Resource not found exception for the id: " + id)));
+        try {
+            return ResponseEntity
+                    .ok()
+                    .body(roleService.getRoleById(id).orElseThrow(() -> new IllegalArgumentException("Resource not found exception for the id: " + id)));
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
     }
 
     @PostMapping
-    public ResponseEntity<RoleDTO> create(@RequestBody final RoleDTO dto) throws URISyntaxException {
-        if (dto.getId() != null) {
-            throw new IllegalArgumentException("A new role cannot already have an id.");
+    public ResponseEntity<RoleDTO> create(@RequestBody final RoleDTO dto) {
+        try {
+            if (dto.getId() != null) {
+                throw new IllegalArgumentException("A new role cannot already have an id.");
+            }
+
+            RoleDTO role = roleService.save(dto);
+
+            return ResponseEntity.created(new URI("/v1/roles/" + role.getId())).body(role);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
-
-        RoleDTO role = roleService.save(dto);
-
-        return ResponseEntity.created(new URI("/v1/roles/" + role.getId())).body(role);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<RoleDTO> edit(@RequestBody final RoleDTO dto,
-                                                @PathVariable final Long id) throws URISyntaxException {
-        if (dto.getId() == null) {
-            throw new IllegalArgumentException("Invalid role id, null value");
-        }
-        if (!Objects.equals(dto.getId(), id)) {
-            throw new IllegalArgumentException("Invalid id");
-        }
+                                                @PathVariable final Long id) {
+        try {
+            if (dto.getId() == null) {
+                throw new IllegalArgumentException("Invalid role id, null value");
+            }
+            if (!Objects.equals(dto.getId(), id)) {
+                throw new IllegalArgumentException("Invalid id");
+            }
 
-        return ResponseEntity
-                .ok()
-                .body(this.roleService.save(dto));
+            return ResponseEntity
+                    .ok()
+                    .body(this.roleService.save(dto));
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable final Long id) {
-        roleService.delete(id);
+        try {
+            roleService.delete(id);
 
-        return ResponseEntity.noContent().build();
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
     }
 }
